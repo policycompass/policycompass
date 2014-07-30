2900ac821bc561ae7d1eedd000a94ed602855519
Joscha Krutzki <joka@jokasis.de>
2014-07-30 19:12:23 +0200

    add mockserver for adhocracy auth

--- tests-mock-services/policycompass_adhocracy.py
+++ tests-mock-services/policycompass_adhocracy.py
@@ -1,13 +1,15 @@
-"""Example dummy rest server."""
+"""Adhocracy services dummy rest server."""
 from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.view import view_config
 
 
-@view_config(request_method='GET', route_name='login_email', renderer='json')
-@view_config(request_method='GET', route_name='login_username', renderer='json')
+@view_config(request_method='POST', route_name='login_email',
+             renderer='json', accept="application/json")
+@view_config(request_method='POST', route_name='login_username',
+             renderer='json', accept="application/json")
 def get_login(request):
-    """Return dummy test data for get to /login_email and /login_username. """
+    """Return dummy test data for get to /login_email and /login_username."""
 
     name = request.json_body['name']
     password = request.json_body['password']
@@ -27,24 +29,10 @@ def get_login(request):
     return success
 
 
-@view_config(request_method='POST', route_name='principals_users', renderer='json')
+@view_config(request_method='POST', route_name='principals_users',
+             renderer='json', accept="application/json")
 def post_principals_users(request):
-    """Return dummy test data for post to /principals_users.
-
-    example post::
-
-    curl -X POST --data-binary @- http://localhost:9901/principals/users <<EOF
-
-    {"content_type": "adhocracy.resources.principal.IUser",
-     "data": {"adhocracy.sheets.user.IUserBasic":
-                {"name": "name",
-                 "email": "email@email.de"},
-              "adhocracy.sheets.user.IPasswordAuthentication":
-                {"password": "password"}
-              }
-     }
-    EOF
-    """
+    """Return dummy test data for post to /principals_users."""
     data = request.json_body
 
     content_type = request.json_body['content_type']
@@ -52,8 +40,8 @@ def post_principals_users(request):
     data = request.json_body['data']
     name = data['adhocracy.sheets.user.IUserBasic']['name']
     email = data['adhocracy.sheets.user.IUserBasic']['email']
-    password = data['adhocracy.sheets.user.IPasswordAuthentication']['password']
-    assert len(password) >= 6
+    passwd = data['adhocracy.sheets.user.IPasswordAuthentication']['password']
+    assert len(passwd) >= 6
 
     error = \
         {'errors': [{'description': 'The user login email is not unique',
@@ -67,7 +55,6 @@ def post_principals_users(request):
     success = {'content_type': 'adhocracy.resources.principal.IUser',
                'path': '/principals/users/000001'}
     return success
-
 
 
 if __name__ == '__main__':
