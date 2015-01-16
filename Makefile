29c1b57cec8908547b2d4a532924115dc48e807c
Nicolas Dietrich <nidi@mailbox.org>
2015-01-16 15:32:29 +0100

    Add policycompass-services settings config to meta repo

--- Makefile
+++ Makefile
@@ -50,7 +50,7 @@ services_install:
 	cd policycompass-services/ ;\
 	./bin/pip3.4 install --upgrade wheel ;\
 	./bin/pip3.4 install --download-cache ../cache/downloads -r requirements.txt ;\
-	cp config/settings.sample.py config/settings.py ;\
+	cp ../etc/services-settings.py config/settings.py ;\
 	bin/python3.4 manage.py migrate ;\
 	bin/python3.4 manage.py loaddata metrics events common references visualizations;\
 
---
+++ etc/services-settings.py
@@ -0,0 +1,40 @@
+__author__ = 'fki'
+
+from .settings_basic import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'pcompass.db',
+    }
+}
+
+# Database config example for PostgreSQL
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'pcompass',
+#         'USER': 'pcompass',
+#         'PASSWORD': 'password',
+#         'HOST': 'localhost',
+#     }
+# }
+
+ELASTICSEARCH_URL = 'http://localhost:9200/policycompass_search/'
+
+PC_SERVICES = {
+    'references': {
+        'base_url': 'http://localhost:8000',
+        'units': '/api/v1/references/units',
+        'external_resources': '/api/v1/references/externalresources',
+        'languages': '/api/v1/references/languages',
+        'domains': '/api/v1/references/policydomains',
+        'dateformats': '/api/v1/references/dateformats',
+        'eventsInVisualizations': '/api/v1/visualizationsmanager/eventsInVisualizations',   
+        'metricsInvisualizations': '/api/v1/visualizationsmanager/metricsInVisualizations',
+        'updateindexitem' : '/api/v1/searchmanager/updateindexitem',
+        'deleteindexitem' : '/api/v1/searchmanager/deleteindexitem',
+        'fcm_base_url': 'http://localhost:8080'
+    }
+}
