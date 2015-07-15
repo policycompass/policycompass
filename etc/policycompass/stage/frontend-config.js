26c661abc923ca87486693ea20f9704ed05f6ecd
Yves Müller <yves.mueller@liqd.de>
2015-07-15 15:09:00 +0200

    Fix hostnames for stage

--- etc/policycompass/stage/frontend-config.js
+++ etc/policycompass/stage/frontend-config.js
@@ -10,16 +10,16 @@
 // Configuration for remote services
 var policyCompassConfig = {
     'URL': '/api/v1',
-    'METRICS_MANAGER_URL': 'https://services-dev.policycompass.eu/api/v1/metricsmanager',
-    'VISUALIZATIONS_MANAGER_URL': 'https://services-dev.policycompass.eu/api/v1/visualizationsmanager',
-    'EVENTS_MANAGER_URL': 'https://services-dev.policycompass.eu/api/v1/eventsmanager',
-    'REFERENCE_POOL_URL': 'https://services-dev.policycompass.eu/api/v1/references',
+    'METRICS_MANAGER_URL': 'https://services-stage.policycompass.eu/api/v1/metricsmanager',
+    'VISUALIZATIONS_MANAGER_URL': 'https://services-stage.policycompass.eu/api/v1/visualizationsmanager',
+    'EVENTS_MANAGER_URL': 'https://services-stage.policycompass.eu/api/v1/eventsmanager',
+    'REFERENCE_POOL_URL': 'https://services-stage.policycompass.eu/api/v1/references',
     'FCM_URL': 'https://alpha.policycompass.eu/api/v1/fcmmanager',
-    'ELASTIC_URL' : 'https://search-dev.policycompass.eu/',
+    'ELASTIC_URL' : 'https://search-stage.policycompass.eu/',
     'ELASTIC_INDEX_NAME' : 'policycompass_search',
     'ENABLE_ADHOCRACY': true,
-    'ADHOCRACY_BACKEND_URL': 'https://adhocracy-backend-dev.policycompass.eu',
-    'ADHOCRACY_FRONTEND_URL': 'https://adhocracy-frontend-dev.policycompass.eu'
+    'ADHOCRACY_BACKEND_URL': 'https://adhocracy-frontend-stage.policycompass.eu/api',
+    'ADHOCRACY_FRONTEND_URL': 'https://adhocracy-frontend-stage.policycompass.eu'
 };
 
 angular.module('pcApp.config', []).constant('API_CONF', policyCompassConfig);
