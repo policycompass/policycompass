11053fc1f4d8e5abd86343aa58854763a50c6c0a
Fabian Kirstein <fabi.kirstein@gmail.com>
2016-09-26 23:09:38 +0200

    Added Eventminer URL

--- etc/policycompass/prod/services-settings.py
+++ etc/policycompass/prod/services-settings.py
@@ -33,7 +33,8 @@ PC_SERVICES = {
         'updateindexitem' : '/api/v1/searchmanager/updateindexitem',
         'deleteindexitem' : '/api/v1/searchmanager/deleteindexitem',
         'fcm_base_url': 'http://localhost:10080',
-        'adhocracy_api_base_url': 'https://adhocracy-prod.policycompass.eu/api'
+        'adhocracy_api_base_url': 'https://adhocracy-prod.policycompass.eu/api',
+        'eventminer_url': 'http://localhost:5000/extraction'
     },
     'external_resources': {
         'physical_path_phantomCapture': '/home/policycompass/policycompass/policycompass-services/apps/visualizationsmanager/phantomCapture/main.js',
